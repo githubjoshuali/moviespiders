cf4cf1033bc5631cd92b125effaf84cc0949d2ac
githubjoshuali <gjoshualee@gmail.com>
2017-05-17 15:44:02 +0800

    Update ljcj.py

--- ljhouse/ljcj/spiders/ljcj.py
+++ ljhouse/ljcj/spiders/ljcj.py
@@ -16,32 +16,30 @@ class LjcjSpider(CrawlSpider):
             callback="parse_ljcj", follow=True),
     )
     def parse_ljcj(self, response):
-        item = LjcjItem()
+        for sel in response.xpath("//ul[@class='listContent']/li"):
+            item = LjcjItem()
+            sel.get_name(response, item)
+            sel.get_url(response, item)
+            
+            def get_name(self, response, item):
+                name = response.xpath("div[@class='info']/div[@class='title']/text()").extract()
+                if name: item["name"] = name[0].strip()
+                    
+            def get_url(self, response, item):
+                url = response.xpath("div[@class='info']/div[@class='title']/a/@href").extract()
+                if url: item["url"] = url[0].strip()
+                    
+            yield item
 
-        housecount = int(response.xpath("//div[@class='resultDes clear']/div[@class='total fl']/span/text()"))
-        if housecount: pagecount = housecount%30 + 1
-        self.get_name(response, item)
-        self.get_url(response,item)
-        housecount = []
-        pagecount = []
-
-        return item
-
-    def get_name(self, response, item):
-        name = response.xpath("//ul[@class='listContent']/li[]/div[@class='info']/div[@class='title']/text()").extract()
-        if name: item["name"] = name[0].strip()
-
-    def get_url(self, response, item):
-        url = response.xpath("//ul[@class='listContent']/li[]/div[@class='info']/div[@class='title']/a").extract()
-        if url: item["url"] = url[0].strip()            
+        return item        
 
 class LjcjReviewSpider(CrawlSpider):
     name = "ljcj_review"
     allowed_domains = ["bj.lianjia.com"]
-    start_urls = ["http://bj.lianjia.com/chengjiao/"]
+    start_urls = ["http://bj.lianjia.com/chengjiao/ny1sf1lc1lc2lc3f1f2f5y2y3y4y1l2l1a2a1p2p1/"]
 
     rules = (
-        Rule(LinkExtractor(allow=r"/chengjiao/\w+\.html"),
+        Rule(LinkExtractor(allow=r"/chengjiao/\w+\/ny1sf1lc1lc2lc3f1f2f5y2y3y4y1pg\d+l2l1a2a1p2p1/"),
             callback="parse_review", follow=True),
     )
 
